b23227f588642892bb9aa3d150cf800d74b540f0
Alawndus Davis, EMT <alawndus@hotmail.com>
2026-07-13 21:04:41 -0600

    Add centralized VCC client configuration

---
+++ config.js
@@ -0,0 +1,18 @@
+export const VCC_CONFIG = Object.freeze({
+  company: "VILIGANS COMMAND CORPORATION",
+  division: "DOCS NREMT & Logistics",
+  corporateEmail: "aldavis@viliganscommandcorp.com",
+  website: "https://viliganscommandcorp.com",
+  amplitude: Object.freeze({
+    apiKey: "8ea17b30e1283120fab527855172550d",
+    autocapture: true,
+    sessionReplaySampleRate: 1,
+  }),
+});
+
+export const VCC_ANALYTICS_CONTEXT = Object.freeze({
+  company: VCC_CONFIG.company,
+  division: VCC_CONFIG.division,
+  corporate_email: VCC_CONFIG.corporateEmail,
+  website: VCC_CONFIG.website,
+});
